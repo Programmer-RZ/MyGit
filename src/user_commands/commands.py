1db17be44980d9b77f945320a0bdd90a0a43c52b
Programmer-RZ <123858154+Programmer-RZ@users.noreply.github.com>
2023-05-05 14:10:16 -0400

    Users can create and run their own commands

--- src/user_commands/commands.py
+++ src/user_commands/commands.py
@@ -18,7 +18,7 @@ class AddCommands(Repository):
         # create a new .mygit script with the appropiate name
         # get name of file in path
         file = name
-        scripts_path = os.path.abspath(os.join(os.path.dirname(__file__), "mygit_scripts"))
+        scripts_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "mygit_scripts"))
         with open(f"{scripts_path}\\{file}", "w") as script:
             # copy contents of user inputed file to script
             user_script = open(f"{path}\\{name}", "r")
@@ -48,5 +48,26 @@ class CreateTemplate(Repository):
     def run(self, path, arguments, tags, repo):
         self.repo = repo
 
+        self.create(arguments[0])
+
+        command_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "mygit_scripts", f"{arguments[0]}.py"))
+
+        print(f"Command Template created at {command_path}")
+
         return repo
+    
+    def create(self, name):
+        scripts_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "mygit_scripts"))
         
+        # read the data of template
+        data = []
+        with open(f"{scripts_path}\\..\\template.txt", "r") as template:
+            data = template.readlines()
+        
+        # change the template data depending on the name of the command
+        data[4] = f"class {name}(Repository):\n"
+        data[14] = f"    {name}.perform({name}, path, arguments, tags, repo)"
+
+        # update the command data
+        with open(f"{scripts_path}\\{name}.py", "w") as command:
+            command.writelines(data)
